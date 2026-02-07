170efd52f767953c8d8030b7c2f498b8bf027c84
SunnyDev <132056995+Sunnyk47@users.noreply.github.com>
2026-02-07 21:55:52 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
 print("Wassup Buddy!! v11")
+
+num1 = int(input("Please enter your first number: "))
+num2 = int(input("Please enter your second number: "))
+
+if num2 > num1:
+    print(f"{num2} is greater than {num1}.")
+else:
+    print(f"{num1} is greater than {num2}.")
